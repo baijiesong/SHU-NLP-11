e644de4b4278cca72b412c3f3fcef9c649289b7b
Jiesong Bai <105435837+baijiesong@users.noreply.github.com>
2024-10-16 17:34:00 +0800

    add ml

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import torch
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import AutoTokenizer,DebertaForSequenceClassification
 from torch.nn.functional import softmax
 import os
 import time
 def load_model(model_path, device):
     # 加载预训练的 BERT 模型结构，设置分类任务的输出标签数量为 2（可以根据你的任务调整）
-    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+    model = DebertaForSequenceClassification.from_pretrained("microsoft/deberta-base", num_labels=2)
     # 加载自定义的模型权重
     model.load_state_dict(torch.load(model_path),strict=False)
     # 将模型移动到 GPU 或 CPU
@@ -41,84 +41,84 @@ def read_file(file_path):
 
 
 if __name__ == '__main__':
-    model_path = 'E://github_project//model_weights.pth'  # 你保存的模型权重文件
+    model_path = 'E://github_project//deberta_model_v1.pth'  # 你保存的模型权重文件
     device = torch.device('cpu')
     # 加载模型
     model = load_model(model_path, device)
     
     # 加载 BERT 预训练的 tokenizer
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',max_length=2100)
-    # 提示用户输入要处理的文件夹路径
-    folder_path = input("请输入要处理的文件夹路径：")
-    # 初始化统计变量
-    spam_count = 0
-    ham_count = 0
-    total_files = 0
-    sum_count = 0
-    try:
-        # 遍历文件夹中的所有文件
-        for filename in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, filename)
-            
-            # 检查是否为文件（忽略子文件夹）
-            if os.path.isfile(file_path):
-                total_files += 1
-                
-                # 读取文件内容
-                
-                input_text = read_file(file_path) 
-
-                # 打印读取的文件内容
-                #print(f"正在处理文件: {filename}")
-                #print(f"文件内容:\n{input_text}")
-
-                # 进行预测
-                probability, pred_class = predict_spam(input_text)
-
-                # 根据预测结果统计垃圾邮件和正常邮件的数量
-                if pred_class == 1:
-                    spam_count += 1
-                    print(f"预测结果: 垃圾邮件 (Spam)")
-                else:
-                    ham_count += 1
-                    print(f"预测结果: 正常邮件 (Ham)")
-                sum_count+=1
-                print(f"处理的了第{sum_count}封\n")
-                time.sleep(0.2)
-                # print(f"垃圾邮件概率: {probability:.4f}\n")
-
-        # 输出最终统计结果
-        print(f"\n处理完成！总文件数: {total_files}")
-        print(f"垃圾邮件个数: {spam_count}")
-        print(f"正常邮件个数: {ham_count}")
-
-    except FileNotFoundError:
-        print(f"文件夹 '{folder_path}' 未找到，请检查路径是否正确。")
-    except Exception as e:
-        print(f"处理文件时发生错误: {e}")
-    # 输入文本
-    
-    
-    # file_path = input("请输入您要处理的邮件内容文件路径：")
-
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+    # # 提示用户输入要处理的文件夹路径
+    # folder_path = input("请输入要处理的文件夹路径：")
+    # # 初始化统计变量
+    # spam_count = 0
+    # ham_count = 0
+    # total_files = 0
+    # sum_count = 0
     # try:
-    #     input_text = read_file(file_path)
+    #     # 遍历文件夹中的所有文件
+    #     for filename in os.listdir(folder_path):
+    #         file_path = os.path.join(folder_path, filename)
+            
+    #         # 检查是否为文件（忽略子文件夹）
+    #         if os.path.isfile(file_path):
+    #             total_files += 1
+                
+    #             # 读取文件内容
+                
+    #             input_text = read_file(file_path) 
 
-    #     # 打印读取的文件内容
-    #     print(f"文件内容:\n{input_text}")
+    #             # 打印读取的文件内容
+    #             #print(f"正在处理文件: {filename}")
+    #             #print(f"文件内容:\n{input_text}")
 
-    #     # 进行预测
-    #     probability, pred_class = predict_spam(input_text)
-    #     # 输出结果
-    #     print(f"Input Text: {input_text}")
-    #     print(f"Probability of Spam: {probability:.4f}")
-    #     print(f"class: {pred_class}")
-    #     if pred_class == 1:
-    #         print("Predicted as Spam")
-    #     else:
-    #         print("Predicted as Ham")
+    #             # 进行预测
+    #             probability, pred_class = predict_spam(input_text)
+
+    #             # 根据预测结果统计垃圾邮件和正常邮件的数量
+    #             if pred_class == 1:
+    #                 spam_count += 1
+    #                 print(f"预测结果: 垃圾邮件 (Spam)")
+    #             else:
+    #                 ham_count += 1
+    #                 print(f"预测结果: 正常邮件 (Ham)")
+    #             sum_count+=1
+    #             print(f"处理的了第{sum_count}封\n")
+    #             time.sleep(0.2)
+    #             # print(f"垃圾邮件概率: {probability:.4f}\n")
+
+    #     # 输出最终统计结果
+    #     print(f"\n处理完成！总文件数: {total_files}")
+    #     print(f"垃圾邮件个数: {spam_count}")
+    #     print(f"正常邮件个数: {ham_count}")
 
     # except FileNotFoundError:
-    #     print(f"文件 '{file_path}' 未找到，请检查路径是否正确。")
+    #     print(f"文件夹 '{folder_path}' 未找到，请检查路径是否正确。")
     # except Exception as e:
-    #     print(f"读取文件时发生错误: {e}")
+    #     print(f"处理文件时发生错误: {e}")
+    #输入文本
+    
+    
+    input_text = input("请输入您要处理的文件：")
+
+    try:
+        # input_text = read_file(file_path)
+
+        # 打印读取的文件内容
+        print(f"文件内容:\n{input_text}")
+
+        # 进行预测
+        probability, pred_class = predict_spam(input_text)
+        # 输出结果
+        print(f"Input Text: {input_text}")
+        print(f"Probability of Spam: {probability:.4f}")
+        print(f"class: {pred_class}")
+        if pred_class == 1:
+            print("Predicted as Spam")
+        else:
+            print("Predicted as Ham")
+
+    except FileNotFoundError:
+        print(f"文件 '{input_text}' 未找到，请检查路径是否正确。")
+    except Exception as e:
+        print(f"读取文件时发生错误: {e}")
